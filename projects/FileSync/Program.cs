cffa7524b27eec23844648d1408b67f9270c78ad
Jared McKneely <jared.mckneely@gmail.com>
2025-06-06 10:57:11 -0600

    Added entry and exit statements, and also program exit with code 0

--- projects/FileSync/Program.cs
+++ projects/FileSync/Program.cs
@@ -3,7 +3,13 @@ using System;
 
 class FileSync
 {
-    static void Main(string[] args) {
-        //Console.WriteLine("Hello, World!");
+    static void Main(string[] args)
+    {
+        // Entry statement
+        Console.WriteLine("FileSync::Main(): Start");
+
+        // Exit statement
+        Console.WriteLine("FileSync::Main(): End");
+        Environment.Exit(0);
     }
 }
